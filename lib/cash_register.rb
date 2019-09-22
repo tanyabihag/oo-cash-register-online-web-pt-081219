430aa0d2ad76633c56f1df443d72269ba8626fc4
Tanya Bihag <tbihag@gmail.com>
2019-09-22 01:52:40 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,17 +20,5 @@ class CashRegister
       end
   end
 
-  def apply_discount
-    if @discount > 0
-      @total = @total-(@total*(@discount/100.0))
-      @msg = "After the discount, the total comes to $%d." % @total.round
-    else
-      @msg = "There is no discount to apply."
-    end
-  end
-
-  def void_last_transaction
-    @total -= @price
-  end
 
 end
