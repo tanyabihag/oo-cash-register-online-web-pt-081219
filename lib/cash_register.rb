b180b8c5f5200e1bbf2793326d3da99bf7e06640
Tanya Bihag <tbihag@gmail.com>
2019-09-22 01:45:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,14 +8,18 @@ def initiliazation (discount = 0)
   @items = []
 end
 
-def add_item(item, price, quantity = 1)
-  @price = price
-  @total +=price * quantity
+  def add_item(item, price, quantity = 1)
+    @price = price
+    @total += price * quantity
     if quantity > 1
       counter = 0
       while counter < quantity
         @items << item
         counter += 1
       end
+    else
+      @items << item
     end
+  end
+
   end
