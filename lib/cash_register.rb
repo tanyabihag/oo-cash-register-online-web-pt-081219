1562161df8e3f08c5b13c22d2e9d3df84dfe936e
Tanya Bihag <tbihag@gmail.com>
2019-09-22 01:32:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
 
-attr_accessor :total, :discount
+attr_accessor :total, :discount, :item, :price
   
 def initiliazation (discount = 0)
   @total = 0
   @discount = discount
 end
 
-def add_item(item, price, quantity = 0)
+def add_item(item, price, quantity = 1)
 end
